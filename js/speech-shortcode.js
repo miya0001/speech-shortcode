61c0b74dc1a3fc6434e3753b74a1432e3209f5c7
miya0001 <miya@wpist.me>
2017-02-11 08:06:46 +0900

    bug fix on voice

--- js/speech-shortcode.js
+++ js/speech-shortcode.js
@@ -1,16 +1,17 @@
+var voices = speechSynthesis.getVoices();
 document.addEventListener( "DOMContentLoaded", function( event ) {
 	var speeches = document.querySelectorAll('.speech-shortcode');
 	for ( var i = 0; i < speeches.length; i++ ) {
 		speeches[ i ].addEventListener( 'click', function() {
 			var voice = this.getAttribute( 'data-voice' );
-			var lang = this.getAttribute( 'data-lang' );
-			var text = this.innerText;
 
-			var synthes = new SpeechSynthesisUtterance( text );
-			synthes.lang = 'lang';
+			var synthes = new SpeechSynthesisUtterance( this.innerText );
+			synthes.lang = this.getAttribute( 'data-lang' );
 			var voices = speechSynthesis.getVoices();
 			voices.forEach( function( v, i ) {
-				if ( v.name == voice ) synthes.voice = v;
+				if ( v.name == voice ) {
+					synthes.voice = v;
+				}
 			} );
 			speechSynthesis.speak( synthes );
 		}, false );
