f0fd1de823973a21c120d2899a1082b498824732
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-25 01:37:15 +0900

    load css dynamically

--- speech-shortcode.php
+++ speech-shortcode.php
@@ -16,6 +16,8 @@ require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
 
 class Speech_Shortcode
 {
+	const version = 'nightly';
+
 	public function __construct()
 	{
 		add_action( 'init', array( $this, 'activate_autoupdate' ) );
@@ -40,17 +42,6 @@ class Speech_Shortcode
 			'rate' => apply_filters( 'speech_shortcode_default_rate', '1' ),
 		), $atts, 'speech' );
 
-		return sprintf(
-			'<span class="speech-shortcode" data-lang="%1$s" data-voice="%2$s" data-rate="%3$s">%4$s</span>',
-			esc_attr( $atts['lang'] ),
-			esc_attr( $atts['voice'] ),
-			esc_attr( $atts['rate'] ),
-			wp_strip_all_tags( $content )
-		);
-	}
-
-	public function wp_enqueue_scripts()
-	{
 		wp_enqueue_script(
 			'speech-shortcode',
 			plugins_url( 'js/speech-shortcode.min.js', __FILE__ ),
@@ -59,14 +50,20 @@ class Speech_Shortcode
 			true
 		);
 
-		wp_enqueue_style( 'dashicons' );
-
-		wp_enqueue_style(
-			'speech-shortcode',
-			plugins_url( 'css/speech-shortcode.min.css', __FILE__ ),
-			array(),
-			filemtime( dirname( __FILE__ ) . '/css/speech-shortcode.min.css' )
+		return sprintf(
+			'<span class="speech-shortcode" data-lang="%1$s" data-voice="%2$s" data-rate="%3$s" data-url="%5$s" data-version="%6$s">%4$s</span>',
+			esc_attr( $atts['lang'] ),
+			esc_attr( $atts['voice'] ),
+			esc_attr( $atts['rate'] ),
+			wp_strip_all_tags( $content ),
+			esc_url( untrailingslashit( plugins_url( "", __FILE__ ) ) ),
+			esc_attr( self::version )
 		);
+	}
+
+	public function wp_enqueue_scripts()
+	{
+		wp_enqueue_style( 'dashicons' );
 	}
 }
 
--- src/speech-shortcode.js
+++ src/speech-shortcode.js
@@ -1,47 +1,62 @@
-speechSynthesis.getVoices(); // Pre-load voices.
+( function() {
+	speechSynthesis.getVoices(); // Pre-load voices.
 
-document.addEventListener( "DOMContentLoaded", function( event ) {
-	var speeches = document.querySelectorAll('.speech-shortcode');
-	for ( var i = 0; i < speeches.length; i++ ) {
-		speeches[ i ].addEventListener( 'click', read_text, false );
+	var el = document.querySelector( '.speech-shortcode' );
+	if ( el ) {
+		var url = el.getAttribute( 'data-url' );
+		var ver = el.getAttribute( 'data-version' );
+
+		var link = document.createElement( 'link' );
+		link.setAttribute( 'rel', 'stylesheet' );
+		link.setAttribute( 'type', 'text/css' );
+		link.setAttribute( 'media', 'all' );
+		link.setAttribute( 'href', url + '/css/speech-shortcode.min.css?ver=' + ver );
+		document.head.appendChild( link );
 	}
-} );
 
-var read_text = function( event ) {
-	speechSynthesis.cancel();
-	document.querySelectorAll('.rumble').forEach( function( currentValue ) {
-		currentValue.classList.remove( 'rumble' );
-	} );
-
-	var synthes = new SpeechSynthesisUtterance( this.textContent );
-
-	// `boundary` or `start` doesn't fires with some voice.
-	synthes.addEventListener( 'start', start_rumble( this ), false );
-	synthes.addEventListener( 'boundary', start_rumble( this ), false );
-	synthes.addEventListener( 'end', end_rumble( this ), false );
-
-	synthes.lang = this.getAttribute( 'data-lang' );
-	synthes.rate = parseFloat( this.getAttribute( 'data-rate' ) );
-
-	var voice = this.getAttribute( 'data-voice' );
-	var voices = speechSynthesis.getVoices();
-	voices.forEach( function( v, i ) {
-		if ( v.name == voice ) {
-			synthes.voice = v;
+	document.addEventListener( "DOMContentLoaded", function( event ) {
+		var speeches = document.querySelectorAll( '.speech-shortcode' );
+		for ( var i = 0; i < speeches.length; i++ ) {
+			speeches[ i ].addEventListener( 'click', read_text, false );
 		}
 	} );
 
-	speechSynthesis.speak( synthes );
-}
+	var read_text = function( event ) {
+		speechSynthesis.cancel();
+		document.querySelectorAll('.rumble').forEach( function( currentValue ) {
+			currentValue.classList.remove( 'rumble' );
+		} );
 
-var start_rumble = function( element ) {
-	return function( event ) {
-		element.classList.add( 'rumble' );
-	}
-}
+		var synthes = new SpeechSynthesisUtterance( this.textContent );
 
-var end_rumble = function( element ) {
-	return function( event ) {
-		element.classList.remove( 'rumble' );
+		// `boundary` or `start` doesn't fires with some voice.
+		synthes.addEventListener( 'start', start_rumble( this ), false );
+		synthes.addEventListener( 'boundary', start_rumble( this ), false );
+		synthes.addEventListener( 'end', end_rumble( this ), false );
+
+		synthes.lang = this.getAttribute( 'data-lang' );
+		synthes.rate = parseFloat( this.getAttribute( 'data-rate' ) );
+
+		var voice = this.getAttribute( 'data-voice' );
+		var voices = speechSynthesis.getVoices();
+		voices.forEach( function( v, i ) {
+			if ( v.name == voice ) {
+				synthes.voice = v;
+			}
+		} );
+
+		speechSynthesis.speak( synthes );
 	}
-}
+
+	var start_rumble = function( element ) {
+		return function( event ) {
+			element.classList.add( 'rumble' );
+		}
+	}
+
+	var end_rumble = function( element ) {
+		return function( event ) {
+			element.classList.remove( 'rumble' );
+		}
+	}
+} )();
